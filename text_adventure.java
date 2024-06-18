f3e279f18b93336859e3a60a9f02047220cdb529
slushy <107578323+blue-slushy9@users.noreply.github.com>
2024-06-18 15:37:12 -0700

    Update text_adventure.java

--- text_adventure.java
+++ text_adventure.java
@@ -2,18 +2,23 @@
 import java.util.Scanner;
 
 
-// Define your character traits
+// Contains methods and attributes pertaining to character traits
 public class CreateCharacter {
     // Define class that creates Scanner object
     public void() {
     // Create Scanner object
     Scanner scanner = new Scanner(System.in);
     }
-    // Define character's name
-    public String() {
-        System.out.println("Please enter your character's name: ");
+    // Receive character name from user
+    public String getName() {
+        // Unlike 'println', 'print' leaves cursor on same line
+        System.out.print("Please enter your character's name: ");
+        // 'nextLine()' 
         String name = scanner.nextLine();
-        System.out.println("Your character's name is " + name + ". Is this correct? [Y/n]");
+        // Confirm name entered is correct
+        System.out.println("You have entered " + name + ", is this correct? [Y/n]");
+        // 'charAt(0)' reads only the first character entered
+        char confirm = scanner.next().charAt(0);
     }
 }
 
