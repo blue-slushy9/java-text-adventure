cd287fe7bf2db4619913b1f9c4ee9f9af54890b4
slushy <107578323+blue-slushy9@users.noreply.github.com>
2024-07-21 23:20:04 +0000

    still working through the compile errors

--- text_adventure.java
+++ text_adventure.java
@@ -15,20 +15,20 @@ public class InputAndConfirm {
     // Define constructor
     public InputAndConfirm(String pleaseEnter, String isCorrect, String okay) {
         // Using 'this' ensures the variables/fields will be specific to the object instance
-	this.pleaseEnter = pleaseEnter;
-	this.isCorrect = isCorrect;
-	this.okay = okay;
+		this.pleaseEnter = pleaseEnter;
+		this.isCorrect = isCorrect;
+		this.okay = okay;
     }
 
     // Define method for getting user input
     public static String getInput(String pleaseEnter) {
-	// Create new Scanner object // 'System.in'
+		// Create new Scanner object // 'System.in'
         Scanner scanner = new Scanner(System.in);
-	// Print your message that prompts the user
-	System.out.println(pleaseEnter);
-	// This string will store the user input, which we receive with scanner
-	String input = scanner.nextLine();
-	return input;
+		// Print your message that prompts the user
+		System.out.println(pleaseEnter);
+		// This string will store the user input, which we receive with scanner
+		String input = scanner.nextLine();
+		return input;
     }
 
     // Define method for confirming user input
@@ -36,36 +36,36 @@ public class InputAndConfirm {
         // Create new Scanner object // 'System.in' 
         Scanner scanner = new Scanner(System.in);
         // Print your message that prompts the user
-	System.out.println(isCorrect + " [Y/n] ");
-	// Read user input and assign to variable // trim() excises extraneous input text
-	String yesOrNo = scanner.nextLine().trim();
-	// If user entered 'Y' or 'y'...
-	if (yesOrNo.equalsIgnoreCase("y")) {
-	    // ...print the confirmation of the input
-	    System.out.println(okay);
-	    return
-	// Else, if the user entered 'N' or 'n'...
-	} else if (yesOrNo.equalsIgnoreCase("n")) {
-	    // Call function recursively until user enters correct string
-	    getConfirmation(isCorrect, okay);
-	} else {
-	    System.out.println("Sorry, that input isn't valid. Please try again.");
-	    getConfirmation(isCorrect, okay);
-	}
+		System.out.println(isCorrect + " [Y/n] ");
+		// Read user input and assign to variable // trim() excises extraneous input text
+		String yesOrNo = scanner.nextLine().trim();
+		// If user entered 'Y' or 'y'...
+		if (yesOrNo.equalsIgnoreCase("y")) {
+			// ...print the confirmation of the input
+			System.out.println(okay);
+			return true;
+		// Else, if the user entered 'N' or 'n'...
+		} else if (yesOrNo.equalsIgnoreCase("n")) {
+			// Call function recursively until user enters correct string
+			getConfirmation(isCorrect, okay);
+		} else {
+			System.out.println("Sorry, that input isn't valid. Please try again.");
+			getConfirmation(isCorrect, okay);
+		}
     }
 
     // Method for verifying user input against an array
     public static boolean isInArray(String[] array, String userInput) {
-	// for each element in the array
+	    // for each element in the array
         for (String element : array) {
-	    if (element.equals(userInput)) {
-	        return true;
+			if (element.equals(userInput)) {
+				return true;
+			}
 	    }
-	}
-	// False goes after the for loop because we want to finish iterating
-	// over the entire array before returning false, since return stops
-	// the iteration
-	return false;
+		// False goes after the for loop because we want to finish iterating
+		// over the entire array before returning false, since return stops
+		// the iteration
+		return false;
     }
 	    
 }
@@ -75,26 +75,27 @@ public class CreateCharacter {
 	
     // Method for receiving character name from user
     public String getName() {
-	// Define our three strings for this method
-	String enterName = "Please enter your character's name: ";
-	String correctName = "You have entered "+name+" for your character's name, is this correct?";
-	String yourName = "Okay, your character's name will be "+name+".";
-        // Create instance of InputAndConfirm class, call getInput method for character name
-	String name = new InputAndConfirm.getInput(enterName);
-        // Confirm user input for character name
-	new getConfirmation(correctName, yourName);
-        return name
+		// Define our three strings for this method
+		String enterName = "Please enter your character's name: ";
+		String correctName = "You have entered "+name+" for your character's name, is this correct?";
+		String yourName = "Okay, your character's name will be "+name+".";
+		
+		// Create instance of InputAndConfirm class, call getInput method for character name
+		String name = new InputAndConfirm.getInput(enterName);
+		// Confirm user input for character name
+		new getConfirmation(correctName, yourName);
+		return name;
     }
 
     // Method for receiving character race from user
     public String getRace() {
 	    
-	// Define array of valid character races
-	String[] races = {"Human", "Elf", "Dwarf", "Troll", "Halfling"};
-	// Define the three standard strings for taking and confirming user input
-    String enterRace = "Please enter your character's race: ";
-	String correctRace = "You have entered "+race+" for your character's race, is this correct?";
-	String yourRace = "Okay, your character's race will be "+race+".";
+		// Define array of valid character races
+		String[] races = {"Human", "Elf", "Dwarf", "Troll", "Halfling"};
+		// Define the three standard strings for taking and confirming user input
+		String enterRace = "Please enter your character's race: ";
+		String correctRace = "You have entered "+race+" for your character's race, is this correct?";
+		String yourRace = "Okay, your character's race will be "+race+".";
 	}
 
 	// Define method that prints the races array to the terminal so user can see
@@ -115,20 +116,19 @@ public class CreateCharacter {
 	// Create new instance of method for verifying whether user input is a valid 
 	// race based on our array
 	boolean valid = new InputAndConfirm.isInArray(races, race);
-	    if (valid == true) {
-	        // Confirm user input for character race
-	        new getConfirmation(correctRace, yourRace);
-	    } else if (valid == false) {
-	        System.out.println("Sorry, that is not a valid selection. Please enter" 
-				 + " one of the following options:"); // '+' is the line continuation
+	if (valid) {
+		// Confirm user input for character race
+		new getConfirmation(correctRace, yourRace);
+	} else if (!valid) {
+		System.out.println("Sorry, that is not a valid selection. Please enter" 
+						 + " one of the following options:"); // '+' is the line continuation
 		printRaces(races);                                    // operator
 		// Call function recursively until user enters valid input
 		boolean valid = new InputAndConfirm.isInArray(races, race);
-	    }
+	}
 		
 	// Confirm user input for character race
 	//new getConfirmation(correctRace, yourRace);
-    }
 }
 
 // Define main class
